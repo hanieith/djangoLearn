3dbca42682b7d9bacd31fb89112ba5635a372472
Constantin Shkutkov <shkutkoff@gmail.com>
2022-01-01 18:15:58 +0300

    01.01.2022

---
+++ hello/firstapp/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("<h2>Глaвнaя</h2>")
+
+def about(request):
+    return HttpResponse("<h2>0 сайте</h2>")
+
+def contact(request):
+    return HttpResponse("<h2>Koнтaкты</h2>")
