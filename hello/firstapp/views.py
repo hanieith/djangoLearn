1a10e66b5e4d078ff55a17ab83a0ea674972e75a
Constantin Shkutkov <shkutkoff@gmail.com>
2022-01-02 21:05:50 +0300

    02.01.2022

--- hello/firstapp/views.py
+++ hello/firstapp/views.py
@@ -9,3 +9,11 @@ def about(request):
 
 def contact(request):
     return HttpResponse("<h2>Koнтaкты</h2>")
+
+def products(request, productid=1):
+    output = f"<h2>Продукт № {productid}</h2>"
+    return HttpResponse(output)
+
+def users(request, id, name):
+    output = f"<h2>Пользователь</h2><h3>id:{id}</h3><h3>name:{name}</h3>"
+    return HttpResponse(output)
--- hello/hello/urls.py
+++ hello/hello/urls.py
@@ -22,5 +22,8 @@ urlpatterns = [
     path('', views.index, name='home'),
     re_path(r'^about/contact/', views.contact),
     re_path(r'^about', views.about),
+    re_path(r'^products/$', views.products),
+    re_path(r'^products/(?P<productid>\d+)/', views.products),
+    re_path(r'^users/(?P<id>\d+)/(?P<name>\D+)/', views.users),
     path('admin/', admin.site.urls),
 ]
