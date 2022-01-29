55cc03274de7ab2eaed451b4aa985213ae11bed0
Constantin Shkutkov <shkutkoff@gmail.com>
2022-01-29 15:40:41 +0300

    29.01.2022

--- hello/firstapp/views.py
+++ hello/firstapp/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect, HttpResponsePermanentRedirect
+from django.http import HttpResponse
+from .forms import UserForm
 
 def index(request):
-    return render(request, "firstapp/index.html")
+    if request.method == "POST":
+        name = request.POST.get("name")
+        age = request.POST.get("age")
+        output = f"<h2>Пользователь</h2><h3>Имя - {name}, Возраст - {age}</h3>"
+        return HttpResponse(output)
+    else:
+        userform = UserForm()
+        return render(request, "firstapp/index.html", {"form": userform})
 
 def about(request, productid=1):
     return render(request, "firstapp/about.html")
