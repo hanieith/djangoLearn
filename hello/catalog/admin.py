7f1a284641dfabc6774715b76532216263721a9f
Constantin Shkutkov <shkutkoff@gmail.com>
2022-02-23 23:05:50 +0300

    23.02.2022

--- hello/catalog/admin.py
+++ hello/catalog/admin.py
@@ -26,12 +26,13 @@ class BookAdmin(admin.ModelAdmin):
 
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('book', 'status', 'borrower', 'due_back', 'id')
     list_filter = ('book', 'status')
     fieldsets = (
         ('Экземпляр книги', {
             'fields': ('book', 'imprint', 'inv_nom')
         }),
         ('Статус и окончание его действия', {
-            'fields': ('status', 'due_back')
+            'fields': ('status', 'due_back','borrower')
         }),
     )
--- hello/catalog/models.py
+++ hello/catalog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.urls import reverse
-
+from django.contrib.auth.models import User
+from datetime import date
 
 class Genre(models.Model):
     name = models.CharField(max_length=200,
@@ -99,6 +100,15 @@ class BookInstance(models.Model):
     due_back = models.DateField(null=True, blank=True,
                                 help_text="введите конец срока статуса",
                                 verbose_name="Дата окончания статуса")
+    borrower = models.ForeignKey(User, on_delete=models.SET_NULL,
+                                 null=True, blank=True,
+                                 verbose_name='Заказчик', help_text='Выберите заказчика книги')
 
     def __str__(self):
         return '%s %s %s' % (self.inv_nom, self.book, self.status)
+
+    @property
+    def is_overdue(self):
+        if self.due_back and date.today() > self.due_back:
+            return True
+        return False
